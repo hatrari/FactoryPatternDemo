a1f85383914c5766af4cbadfca08119cea6ea129
hatrari <ham.atrari@gmail.com>
2021-01-31 10:53:29 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoryPatternDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
